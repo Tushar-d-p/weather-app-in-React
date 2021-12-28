12c543c49ab64d9b36202fc957bc819a9843dc90
tushar-d-p <algorithm321@gmail.com>
2021-12-28 23:09:43 +0530

    Basic version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [data,setData] = useState(null);
+
+  const [search, setSearch] = useState('');
+
+  const handleChange=(e)=>{
+    // console.log(e.target.value);
+    setSearch(e.target.value);
+  }
+
+  useEffect(()=>{
+    let result = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=efba5ae56b24058ab111b44c704e3da7`);
+
+    result.then((response)=>{
+        return response.json();
+    }).then((data)=>{
+          setData(data);
+          console.log(data);
+    })
+  },[search])
+
+  // console.log(data.main.temp);
+
+  console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="outerbox">
+      <input type="text" placeholder="Search city worlwide" value={search} onChange={handleChange} />
+      <div className="temperature">
+          { data?.main?.temp?
+            <>
+              <span>Temperature:</span>
+              {data.main.temp}
+              <span>&deg;C</span>
+              <div><span>Humidity:</span>{data.main.humidity}%</div>
+              <div><span>Wind-Speed:</span>{data.wind.speed}Km/h</div>
+              <div><span>Wind-Direction:</span>{data.wind.deg}<span>&#8599;</span></div>
+            </>
+            :
+            null
+          }
+      </div>
+      {/* <div>{data?data.main:null}</div> */}
     </div>
   );
 }
